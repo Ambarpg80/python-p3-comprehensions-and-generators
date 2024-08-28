932fe10f7b9085fa5eba618d0f35924845155dab
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2024-08-28 13:36:08 -0400

    add solution to list comprehension practice

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+#   evens= list()
+#   for n in num_list:
+#     if n % 2 == 0:
+#       evens.append(n)
+#   else: 
+#     print(evens)
+#   return evens 
+    evens = [n for n in num_list if n % 2 ==0]
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    add_exclamation  = [n + "!" for n in sentence_list ]
+    return add_exclamation 
+        
+
+# new_list = [optional_operation(item) for item in old_list if optional_condition == True]
